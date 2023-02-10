76edfc687e56e708de646aeae81d0057ba4aa681
Riccardo P <riccardop@outlook.com>
2023-02-10 18:16:41 +1100

    server: added server.js file with basic server functionality. Updated package.json to include scripts to start server.

---
+++ server/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+async function startServer() {
+    app.listen(PORT, () => {
+        console.log(`Listening on port: ${PORT}`);
+    });
+}
+
+startServer();
