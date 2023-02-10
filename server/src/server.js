6066d680f3245d3d3e4f838ed980e61d82b95aa2
Riccardo P <riccardop@outlook.com>
2023-02-10 18:21:53 +1100

    server: added .env integration into server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
+const results = require('dotenv').config();
+
+if(results.error) {
+    throw results.error;
+}
+
 const express = require('express');
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT_NUMBER || 8000;
 
 async function startServer() {
     app.listen(PORT, () => {
         console.log(`Listening on port: ${PORT}`);
+        console.log(process.env.OPENAI_API_KEY);
     });
 }
 
