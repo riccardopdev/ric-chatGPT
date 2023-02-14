e70981cd007d814a9245a9b2846f3f3300a14368
Riccardo P <riccardop@outlook.com>
2023-02-14 20:03:14 +1100

    client: added axios and functionality to send request to server endpoint

--- client/src/components/ui/Chat.js
+++ client/src/components/ui/Chat.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-class Chat extends React.Component {
-    render() {
-        return (
-            <>
-                <form>
-                    <input type="text"></input>
-                </form>
-            </>
-        )
+const Chat = () => {
+    const [prompt, setPrompt] = useState('');
+
+    const requestEndPoint = 'http://localhost:5000/api/openai';
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(requestEndPoint, {
+                prompt
+            });
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
     }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+            />
+            <button type="submit">Ask me a question</button>
+        </form>
+    );
 }
 
 export default Chat;
