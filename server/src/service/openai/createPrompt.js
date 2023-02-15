380a170498cbebba35f6d39a94a0bdd913cae779
Riccardo P <riccardop@outlook.com>
2023-02-16 10:55:34 +1100

    server: modified createPrompt and its unit test code to remove validation of prompt parameter

--- server/src/service/openai/createPrompt.js
+++ server/src/service/openai/createPrompt.js
@@ -1,10 +1,6 @@
 /*This function take the original prompt, adds the end indicator ' ->', and returns the updated prompt.
-This is necessary for the openai model to understand when the prompt ends*/
+This is necessary for the openai model to understand when the prompt ends and it should start generating the completion*/
 function createPrompt(prompt) {
-    //Validate the prompt parameter
-    if(prompt === undefined) throw new Error('The orignal prompt is undefined.');
-    if(prompt === '' || prompt === ' ') throw new Error('The original prompt is an empty string.');
-
     const promptEndIndicator = ' ->';
 
     return prompt + promptEndIndicator;
--- server/src/service/openai/createPrompt.test.js
+++ server/src/service/openai/createPrompt.test.js
@@ -2,35 +2,16 @@ import { it, expect } from 'vitest';
 import createPrompt from './createPrompt';
 
 it('should return a string', () => {
-    const promptResult = createPrompt('test');
+    const testString = 'test';
+    const promptResult = createPrompt(testString);
 
     expect(promptResult).toBeTypeOf('string');
 });
 
 it('should return a string containg the end indicator " ->"', () => {
-    const promptResult = createPrompt('test');
+    const testString = 'test';
+    const promptResult = createPrompt(testString);
     const endIndicator = ' ->';
 
     expect(promptResult).contains(endIndicator);
-});
-
-it('should throw an error if nothing is passed into createPrompt', () => {
-    const promptResultFn = () => {
-        createPrompt();
-    }
-
-    expect(promptResultFn).toThrow('The orignal prompt is undefined.');
-});
-
-it('should throw an error if an empty string is passed into createPrompt', () => {
-    const promptResultFn1 = () => {
-        createPrompt('');
-    }
-
-    const promptResultFn2 = () => {
-        createPrompt(' ');
-    }
-
-    expect(promptResultFn1).toThrow('The original prompt is an empty string.');
-    expect(promptResultFn2).toThrow('The original prompt is an empty string.');
 });
