addd16be88ca93f02e56d344ed0146e64e14a067
Riccardo P <riccardop@outlook.com>
2023-02-16 12:53:23 +1100

    server: updated openaiRequests.router.js to validate prompt parameter and return some default text values depending on prompt values

--- server/src/routes/openai/openaiRequests.router.js
+++ server/src/routes/openai/openaiRequests.router.js
@@ -4,23 +4,46 @@ const openaiRouter = express.Router();
 const createCompletion = require('../../service/openai/createCompletion');
 
 openaiRouter.get('/', async (req, res) => {
-    return res.status(200).json({message: 'get method for api/openai endpoint'});
+    //Return status code 405 = Method Not Allowed
+    return res.status(405).json({message: 'You need to implement a post request to the api/openai endpoint. The post request must include a body prompt parameter.'});
 });
 
 openaiRouter.post('/', async (req, res) => {
     //Retrieve the prompt parameter from the request body
-    const prompt = req.body.prompt || 'Tell me about your work experience.';
+    let prompt = req.body.prompt;
+    let response;
 
-    //Make the request to openai
-    const response = await createCompletion(prompt);
+    const replyToInvalidPrompt = 'Sorry, that didn\'t seem to work. Would you like to try again with a question regarding my work experience and career?';
+    const replyToInvalidQuestion = 'Please, ask me a question regarding my work experience and career.';
 
-    if(response instanceof Error) {
-        console.log(`Error: ${response.message}`);
-        res.status(400).json({error: response.message});
-    } else {
-        console.log(response.data.choices);
-        res.status(200).json(response.data.choices);
+    //Check if they body request contains a prompt parameter
+    if(prompt === undefined || prompt === null) {
+        return res.status(400).json({error: 'The request body must have a prompt parameter.'});
     }
+
+    prompt = prompt.trim();
+
+    //If the prompt is less than 10 characters, there is a possibility that is not a well formulated question
+    if(prompt.length < 10) {
+        response = {
+            data: {
+                choices: [
+                    {
+                        text: replyToInvalidPrompt
+                    }
+                ]
+            }
+        };
+
+    } else {
+        //Make the request to openai
+        response = await createCompletion(prompt);
+
+        //Check if openai responded with ' ?' which means the user might have asked an improper or not valid question
+        if(response.data.choices[0].text === ' ?') response.data.choices[0].text = replyToInvalidQuestion;
+    }
+
+    return res.status(200).json(response.data.choices);
 });
 
 module.exports = openaiRouter;
