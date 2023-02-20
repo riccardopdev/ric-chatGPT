18edd2899560cbd6df58eebfc8bb2d3740fc1f28
Riccardo P <riccardop@outlook.com>
2023-02-20 15:25:27 +1100

    client: started adding css styling to components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Intro from './ui/Intro';
 import Chat from './ui/Chat';
 
+import './App.css';
+
 class App extends React.Component {
     render() {
         return (
-            <>
+            <div className='main-container'>
                 <Intro />
                 <Chat />
-            </>
+            </div>
         );
     }
 }
