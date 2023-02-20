161464385cf364fa988fabd461442aeabd5e1b6a
Riccardo P <riccardop@outlook.com>
2023-02-20 17:59:47 +1100

    client: added Intro.css file

--- client/src/components/ui/Intro.js
+++ client/src/components/ui/Intro.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import './Intro.css';
+
 const Intro = () => {
     return (
-        <>
+        <section className="intro-section">
             <h1>ric-chatbot</h1>
             <p>This is my personal chatbot assistant.</p>
             <p>You can ask questions reqarding my career and work experience and ric-chatbot should be able to provide most of the answers based on my CV.</p>
             <p>Give it a try, ask something like "Tell me about your work experience."</p>
-        </>
+        </section>
     )
 }
 
