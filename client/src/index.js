78674300286627d0678af73504709b215563b584
Riccardo P <riccardop@outlook.com>
2023-02-20 16:44:39 +1100

    client: added style for index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
+import './index.css';
+
 const container = document.querySelector('#root');
 const root = createRoot(container);
 
