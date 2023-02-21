67ab3c66da6621b5b2e6b087b7cd889e78ceb173
Riccardo P <riccardop@outlook.com>
2023-02-21 15:37:21 +1100

    client: Intro.js and Intro.css changed copy and style

--- client/src/components/ui/Intro.js
+++ client/src/components/ui/Intro.js
@@ -6,9 +6,9 @@ const Intro = () => {
     return (
         <section className="intro-section">
             <h1>ric-chatbot</h1>
-            <p>This is my personal chatbot assistant.</p>
-            <p>You can ask questions reqarding my career and work experience and ric-chatbot should be able to provide most of the answers based on my CV.</p>
-            <p>Give it a try, ask something like "Tell me about your work experience."</p>
+            <p>This is my personal <strong>chatbot assistant</strong>.</p>
+            <p>You can ask questions regarding my <strong>career</strong>, <strong>education</strong>, and <strong>work experience</strong>. <br />ric-chatbot should be able to provide most of the answers based on <strong>my CV</strong>.</p>
+            <p>Give it a try, ask something like: <span>Tell me about your work experience.</span></p>
         </section>
     )
 }
