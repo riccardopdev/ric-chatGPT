a8a3c7b0eb01dc07da312606e2b16f54f6b3f1a7
Riccardo P <riccardop@outlook.com>
2023-03-17 12:21:48 +1100

    client: ChatForm added functionality to submit on key down and set focus to the text area

--- client/src/components/ui/ChatForm.js
+++ client/src/components/ui/ChatForm.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './ChatForm.css';
 
 const ChatForm = (props) => {
     const [text, setText] = useState('');
+    const txtAreaRef = useRef();
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+
         props.onSubmit({text});
         setText('');
+        txtAreaRef.current.focus();
+    }
+
+    const handleKeyDown = (ev) => {
+        if(ev.keyCode === 13) handleSubmit(ev);
     }
 
     return(
-        <form className='chat-form' onSubmit={onSubmit}>
+        <form className='chat-form' onSubmit={handleSubmit}>
             <textarea
+                ref={txtAreaRef}
+                autoFocus
                 name="question"
                 rows="3"
                 placeholder="Type your question here..."
@@ -21,6 +30,7 @@ const ChatForm = (props) => {
                 value={text}
                 wrap="hard"
                 onChange={(e) => setText(e.target.value)}
+                onKeyDown={handleKeyDown}
             />
             <button type="submit">Submit question</button>
         </form>
