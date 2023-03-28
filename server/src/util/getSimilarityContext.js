67b8d2c11fb2b4da83714fbc2f182044888a6702
Riccardo P <riccardop@outlook.com>
2023-03-28 17:56:31 +1100

    server: changed getSimilarityContext.js to implement comparison between embedding values and return the most similar context to the question

--- server/src/util/getSimilarityContext.js
+++ server/src/util/getSimilarityContext.js
@@ -3,14 +3,29 @@
 //If a similarity is found the item's text from the .json file is returned as the "context" for the question to be submitted to openai completion API
 //If not similarity is found null is returned instead of the context
 const loadJSONFile = require('./loadJSONFile');
+const cosineSimilarity = require('./cosineSimilarity');
+
 const jsonFilePath = __dirname + '../../data/contextEmbeddings.json'; //.json file with original context and embeddings values
+const contextValues = {text: '', similarity: 0} //Object to hold initial context values
 
 const getSimilarityContext = async (questionEmbeddingValues) => {
     const contextEmbeddingValues = await loadJSONFile(jsonFilePath);
 
-    return questionEmbeddingValues;
+    //Reset the context values
+    contextValues.text = '';
+    contextValues.similarity = 0;
+
+    //For each item in the contextEmbeddingValues object we do a similarity check against the questionEmbeddingValues
+    Object.keys(contextEmbeddingValues).forEach((item) => {
+        let similarity = cosineSimilarity(contextEmbeddingValues[item].embedding, questionEmbeddingValues.embedding);
+
+        if(similarity > contextValues.similarity) {
+            contextValues.similarity = similarity;
+            contextValues.text = contextEmbeddingValues[item].text;
+        }
+    });
+
+    return contextValues;
 }
-
-
 
 module.exports = getSimilarityContext;
