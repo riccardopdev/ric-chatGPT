01edf88a6fe9f2552966fac65df6d01c1dbcad27
Riccardo P <riccardop@outlook.com>
2023-04-04 14:55:19 +1000

    server: server.js changed .env ENVIRONMENT_PRODUCTION to be a boolean string

--- server/src/server.js
+++ server/src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
-if(process.env.ENVIRONMENT === 'local_dev') {
+if(process.env.ENVIRONMENT_PRODUCTION === 'false') {
     //RUN LOCALLY
     async function startServer() {
         app.listen(PORT, () => {
