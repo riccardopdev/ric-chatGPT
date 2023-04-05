8c2f544171a16123a4b90827220b6028e8ff5ba0
Riccardo P <riccardop@outlook.com>
2023-04-05 12:10:49 +1000

    client: Intro.js updated intro text

--- client/src/components/ui/Intro.js
+++ client/src/components/ui/Intro.js
@@ -6,9 +6,11 @@ const Intro = () => {
     return (
         <section className="intro-section">
             <h1>ric-chatbot</h1>
+            <p>Hi, my name is <strong>Riccardo</strong>.</p>
             <p>This is my personal <strong>chatbot assistant</strong>.</p>
-            <p>You can ask questions regarding my <strong>career</strong>, <strong>education</strong>, and <strong>work experience</strong>. <br />ric-chatbot should be able to provide most of the answers based on <strong>my CV</strong>.</p>
-            <p>Give it a try, ask something like: <span>Tell me about your work experience.</span></p>
+            <p>It's an experimental and fun project that uses <strong>OpenAI API</strong>, <strong>React</strong>, <strong>Node</strong> and <strong>express</strong> in its technology stack.</p>
+            <p>You can ask questions regarding my <strong>career</strong>, <strong>education</strong>, and <strong>work experience</strong>. <br />ric-chatbot will answer to the best of its knowledge based on information from <strong>my CV</strong>.</p>
+            <p>Give it a try, ask something like: <span>Tell me about Riccardo's work experience?</span></p>
         </section>
     )
 }
