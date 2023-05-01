6578dda5515a43d715efeed2ceb670ab321cbadd
Riccardo P <riccardop@outlook.com>
2023-05-01 11:04:44 +1000

    server: createCompletion.js - added console.log for contextResponse, prompt, and response.data

--- server/src/service/openai/createCompletion.js
+++ server/src/service/openai/createCompletion.js
@@ -8,13 +8,18 @@ const createPrompt = require('./createPrompt');
 //This function is responsible to make the createCompletion request to the openai API endpoint
 const createCompletion = async (contextResponse, prompt) => {
 
+    console.log('----------- ContextResponse:', contextResponse);
+    console.log('----------- Prompt:', prompt);
+
     const response = await openai.createCompletion({
         model: process.env.OPENAI_MODEL_DAVINCI,
         prompt: createPrompt(contextResponse, prompt),
-        max_tokens: 256,
+        max_tokens: 512,
         temperature: 0,
         stop: ['\n']
     });
+
+    console.log('---------- Response:', response.data);
 
     return response;
 };
