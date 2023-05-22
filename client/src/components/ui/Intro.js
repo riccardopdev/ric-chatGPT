b3fe5ef345b917b7d91ce15f3a3eafd43a8e5ee0
Riccardo P <riccardop@outlook.com>
2023-05-22 15:02:20 +1000

    client: Intro.js updated the intro text

--- client/src/components/ui/Intro.js
+++ client/src/components/ui/Intro.js
@@ -7,10 +7,10 @@ const Intro = () => {
         <section className="intro-section">
             <h1>ric-chatbot</h1>
             <p>Hi, my name is <strong>Riccardo</strong>.</p>
-            <p>This is my personal <strong>chatbot assistant</strong>.</p>
-            <p>It's an experimental and fun project that uses <strong>OpenAI API</strong>, <strong>React</strong>, <strong>Node</strong> and <strong>express</strong> in its technology stack.</p>
-            <p>You can ask questions regarding my <strong>career</strong>, <strong>education</strong>, and <strong>work experience</strong>. <br />ric-chatbot will answer to the best of its knowledge based on information from <strong>my CV</strong>.</p>
-            <p>Give it a try, ask something like: <span>Tell me about Riccardo's work experience?</span></p>
+            <p>I'd like to introduce you to my personal <strong>chatbot assistant</strong>.</p>
+            <p>This project is an experimental and enjoyable endeavor that utilizes <strong>OpenAI API</strong>, <strong>React</strong>, <strong>Node</strong>, and <strong>Express</strong> in its technology stack.</p>
+            <p>Feel free to ask any questions about my  <strong>career</strong>, <strong>education</strong>, and <strong>work experience</strong>. <br />My chatbot assistant, ric-chatbot, will do its best to provide accurate answers based on the information available in <strong>my CV</strong>.</p>
+            <p>Give it a try, ask something like: <span>What web technologies is Riccardo proficient in?</span></p>
         </section>
     )
 }
