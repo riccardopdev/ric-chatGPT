fdad2581413ee39b31a1acb21afaeac78aee982c
Riccardo P <riccardop@outlook.com>
2024-03-06 11:00:20 +1100

    Updated the OpenAI model to use GPT-3.5-turbo-instruct and removed the deprecated davinci model

--- server/src/service/openai/createCompletion.js
+++ server/src/service/openai/createCompletion.js
@@ -1,6 +1,6 @@
 const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 const createPrompt = require('./createPrompt');
@@ -8,20 +8,20 @@ const createPrompt = require('./createPrompt');
 //This function is responsible to make the createCompletion request to the openai API endpoint
 const createCompletion = async (contextResponse, prompt) => {
 
-    console.log('----------- ContextResponse:', contextResponse);
-    console.log('----------- Prompt:', prompt);
+  // console.log('----------- ContextResponse:', contextResponse);
+  // console.log('----------- Prompt:', prompt);
 
-    const response = await openai.createCompletion({
-        model: process.env.OPENAI_MODEL_DAVINCI,
-        prompt: createPrompt(contextResponse, prompt),
-        max_tokens: 512,
-        temperature: 0,
-        stop: ['\n']
-    });
+  const response = await openai.createCompletion({
+    model: process.env.OPENAI_MODEL_GPT_3_5_TURBO,
+    prompt: createPrompt(contextResponse, prompt),
+    max_tokens: 512,
+    temperature: 0,
+    stop: ['\n']
+  });
 
-    console.log('---------- Response:', response.data);
+  // console.log('---------- Response:', response.data);
 
-    return response;
+  return response;
 };
 
 module.exports = createCompletion;
